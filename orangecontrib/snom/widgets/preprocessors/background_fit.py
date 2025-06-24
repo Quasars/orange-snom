3d53544e8b3cb131bdba9d85207c0db29c280b5e
Gergely Nemeth <ngergihun@gmail.com>
2025-06-24 18:13:37 +0200

    Background fit updated

--- orangecontrib/snom/widgets/preprocessors/background_fit.py
+++ orangecontrib/snom/widgets/preprocessors/background_fit.py
@@ -2,8 +2,9 @@ from AnyQt.QtWidgets import QFormLayout
 
 from orangecontrib.spectroscopy.widgets.preprocessors.utils import BaseEditorOrange
 from orangecontrib.spectroscopy.widgets.gui import lineEditIntRange
+from orangewidget.gui import checkBox
 
-from pySNOM.images import BackgroundPolyFit, DataTypes
+from pySNOM.images import MaskedBackgroundPolyFit, DataTypes
 
 from orangecontrib.snom.preprocess.utils import (
     PreprocessImageOpts2DOnlyWhole,
@@ -12,15 +13,17 @@ from orangecontrib.snom.widgets.preprocessors.registry import preprocess_image_e
 
 
 class BackGroundFit(PreprocessImageOpts2DOnlyWhole):
-    def __init__(self, xorder=1, yorder=1):
+    def __init__(self, xorder=1, yorder=1, use_mask=False):
         self.xorder = xorder
         self.yorder = yorder
+        self.use_mask = use_mask
 
-    def transform_image(self, image, data):
+    def transform_image(self, image, data, mask=None):
         datatype = data.attributes.get("measurement.signaltype", "Phase")
-        d, b = BackgroundPolyFit(
+        mask = mask if self.use_mask else None
+        d = MaskedBackgroundPolyFit(
             xorder=self.xorder, yorder=self.yorder, datatype=DataTypes[datatype]
-        ).transform(image)
+        ).transform(image,mask=mask)
         return d
 
 
@@ -33,12 +36,17 @@ class BackGroundFitEditor(BaseEditorOrange):
 
         self.xorder = 1
         self.yorder = 1
+        self.use_mask = False
 
         form = QFormLayout()
         xorderedit = lineEditIntRange(self, self, "xorder", callback=self.edited.emit)
         yorderedit = lineEditIntRange(self, self, "yorder", callback=self.edited.emit)
+        self.use_mask_chb = checkBox(self, self,"use_mask","Enable",callback=self.edited.emit)
+        
         form.addRow("xorder", xorderedit)
         form.addRow("yorder", yorderedit)
+        form.addRow("Masking", self.use_mask_chb)
+
         self.controlArea.setLayout(form)
 
     def activateOptions(self):
@@ -47,13 +55,15 @@ class BackGroundFitEditor(BaseEditorOrange):
     def setParameters(self, params):
         self.xorder = params.get("xorder", 1)
         self.yorder = params.get("yorder", 1)
+        self.use_mask = params.get("use_mask", False)
 
     @classmethod
     def createinstance(cls, params):
         params = dict(params)
         xorder = float(params.get("xorder", 1))
         yorder = float(params.get("yorder", 1))
-        return BackGroundFit(xorder=xorder, yorder=yorder)
+        use_mask = bool(params.get("use_mask", False))
+        return BackGroundFit(xorder=xorder, yorder=yorder, use_mask=use_mask)
 
     def set_preview_data(self, data):
         if data:
