aac7f04d23c564f56b3a60bf8060cb503f17fde2
Gergely Nemeth <ngergihun@gmail.com>
2025-10-17 16:30:56 +0200

    Only attributes for feature_value_model

--- orangecontrib/snom/widgets/owpreprocessimage.py
+++ orangecontrib/snom/widgets/owpreprocessimage.py
@@ -190,13 +190,7 @@ class OWPreprocessImage(SpectralImagePreprocessReference):
         self.preview_runner = ImagePreviewRunner(self)
 
         self.feature_value_model = DomainModel(
-            order=(
-                DomainModel.ATTRIBUTES,
-                DomainModel.Separator,
-                DomainModel.CLASSES,
-                DomainModel.Separator,
-                DomainModel.METAS,
-            ),
+            order=(DomainModel.ATTRIBUTES),
             valid_types=ContinuousVariable,
         )
 
