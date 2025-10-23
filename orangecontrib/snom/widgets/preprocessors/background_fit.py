6230087bf7fae56e820dfc5134c4a69433aba836
Gergely Nemeth <ngergihun@gmail.com>
2025-10-23 19:14:17 +0200

    Update BackGroundFit

--- orangecontrib/snom/widgets/preprocessors/background_fit.py
+++ orangecontrib/snom/widgets/preprocessors/background_fit.py
@@ -1,5 +1,7 @@
 from AnyQt.QtWidgets import QFormLayout
 
+from orangewidget.gui import comboBox
+
 from orangecontrib.spectroscopy.widgets.preprocessors.utils import BaseEditorOrange
 from orangecontrib.spectroscopy.widgets.gui import lineEditIntRange
 from orangewidget.gui import checkBox
@@ -8,19 +10,21 @@ from pySNOM.images import MaskedBackgroundPolyFit, DataTypes
 
 from orangecontrib.snom.preprocess.utils import (
     PreprocessImageOpts2DOnlyWhole,
+    MaskOptions,
+    transform_mask,
 )
 from orangecontrib.snom.widgets.preprocessors.registry import preprocess_image_editors
 
 
 class BackGroundFit(PreprocessImageOpts2DOnlyWhole):
-    def __init__(self, xorder=1, yorder=1, use_mask=False):
+    def __init__(self, xorder=1, yorder=1, mask_method=False):
         self.xorder = xorder
         self.yorder = yorder
-        self.use_mask = use_mask
+        self.mask_method = mask_method
 
     def transform_image(self, image, data, mask=None):
         datatype = data.attributes.get("measurement.signaltype", "Phase")
-        mask = mask if self.use_mask else None
+        mask = transform_mask(mask=mask, option=MaskOptions[self.mask_method])
         d = MaskedBackgroundPolyFit(
             xorder=self.xorder, yorder=self.yorder, datatype=DataTypes[datatype]
         ).transform(image, mask=mask)
@@ -36,36 +40,41 @@ class BackGroundFitEditor(BaseEditorOrange):
 
         self.xorder = 1
         self.yorder = 1
-        self.use_mask = False
+        self.mask_method = False
 
         form = QFormLayout()
         xorderedit = lineEditIntRange(self, self, "xorder", callback=self.edited.emit)
         yorderedit = lineEditIntRange(self, self, "yorder", callback=self.edited.emit)
-        self.use_mask_chb = checkBox(
-            self, self, "use_mask", "Enable", callback=self.edited.emit
+        self.maskmethod_cb = comboBox(
+            self, self, "mask_method", callback=self.setmethod
         )
+        self.maskmethod_cb.addItems([e.name for e in MaskOptions])
 
         form.addRow("xorder", xorderedit)
         form.addRow("yorder", yorderedit)
-        form.addRow("Masking", self.use_mask_chb)
+        form.addRow("Mask", self.maskmethod_cb)
 
         self.controlArea.setLayout(form)
 
     def activateOptions(self):
         pass  # actions when user starts changing options
+    
+    def setmethod(self):
+        self.mask_method = self.maskmethod_cb.currentText()
+        self.edited.emit()
 
     def setParameters(self, params):
         self.xorder = params.get("xorder", 1)
         self.yorder = params.get("yorder", 1)
-        self.use_mask = params.get("use_mask", False)
+        self.mask_method = params.get("mask_method", "IGNORE")
 
     @classmethod
     def createinstance(cls, params):
         params = dict(params)
         xorder = float(params.get("xorder", 1))
         yorder = float(params.get("yorder", 1))
-        use_mask = bool(params.get("use_mask", False))
-        return BackGroundFit(xorder=xorder, yorder=yorder, use_mask=use_mask)
+        mask_method = params.get("mask_method", "IGNORE")
+        return BackGroundFit(xorder=xorder, yorder=yorder, mask_method=mask_method)
 
     def set_preview_data(self, data):
         if data:
